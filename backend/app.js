9557d81d6f63214b642163091974118474c5bd11
EvyaDev <106523004+EvyaDev@users.noreply.github.com>
2023-11-21 20:31:37 +0200

    added

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
-const express = require("express")
-const cors = require("cors");
-const app = express();
-const { con } = require("./db_connect");
-const { Server } = require("socket.io")
-const http = require("http");
+// const express = require("express")
+// const cors = require("cors");
+// const app = express();
+// const { con } = require("./db_connect");
+// const { Server } = require("socket.io")
+// const http = require("http");
 
 const server = http.createServer(app);
 
